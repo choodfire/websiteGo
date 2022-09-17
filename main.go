47729f42e0095d35a7bc4b4653aed01c16763072
Ruslan <zzzlyka221@gmail.com>
2022-09-17 22:59:19 +0500

    registration done

--- main.go
+++ main.go
@@ -5,10 +5,11 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	// "time"
 )
 
 type User struct {
-	name string
+	name     string
 	password string
 }
 
@@ -16,9 +17,14 @@ type Users struct {
 	Users []User
 }
 
+func (u *Users) AddNewUser(login string, password string) {
+
+}
+
 var usr Users
 
-func checkAccount(writtenLogin string, writtenPassword string) bool {
+func checkLoginInfo(writtenLogin string, writtenPassword string) bool {
+	fmt.Println(usr)
 	for _, user := range usr.Users {
 		if user.name == writtenLogin && user.password == writtenPassword {
 			return true
@@ -28,8 +34,9 @@ func checkAccount(writtenLogin string, writtenPassword string) bool {
 	return false
 }
 
-func checkRegistration(writtenLogin string, writtenPassword string, 
+func checkRegistrationInfo(writtenLogin string, writtenPassword string,
 	writtenPassword2 string) bool {
+	fmt.Println(usr)
 	if writtenPassword != writtenPassword2 {
 		return false
 	}
@@ -44,68 +51,58 @@ func checkRegistration(writtenLogin string, writtenPassword string,
 }
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Main Метод:", r.Method) // получаем информацию о методе запроса
-
 	t, _ := template.ParseFiles("main.html")
 	t.Execute(w, nil)
 }
 
-func loginPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Login Метод:", r.Method) // получаем информацию о методе запроса
+func login(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("login.html")
+	t.Execute(w, nil)
+}
 
-	switch r.Method {
-	case "GET":
+func loginResult(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println("Log", r.Form)
+
+	if checkLoginInfo(r.FormValue("username"), r.FormValue("password")) == true {
+		// redirect to account page
+		http.Redirect(w, r, "/account", http.StatusSeeOther)
+	} else {
+		// fmt.Fprintf(w, "Wrong password!")
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+	}
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("register.html")
+	t.ExecuteTemplate(w, "register.html", nil)
+}
+
+func registerResult(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println("Reg", r.Form)
+
+	if checkRegistrationInfo(r.FormValue("username"), r.FormValue("password"), r.FormValue("password2")) == true {
+		// fmt.Fprintf(w, "Login successfully!")
 		t, _ := template.ParseFiles("login.html")
-		t.Execute(w, nil)
-	case "POST":
-		r.ParseForm()
-
-		if checkAccount(r.FormValue("username"), r.FormValue("password")) == true {
-			// redirect to account page 
-			// http.Redirect(w, r, "/account/", http.StatusSeeOther)
-		} else {
-			fmt.Fprintf(w, "Wrong password!")
-			// http.Redirect(w, r, "/login/", http.StatusSeeOther)
-		}
-
-	default:
-		fmt.Fprintf(w, "Поддерживаются только GET и POST методы")
-	}
-}
-
-func registerPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Register Метод:", r.Method) // получаем информацию о методе запроса
-
-	switch r.Method {
-	case "GET":
+		t.ExecuteTemplate(w, "login.html", "Registration successful!")
+	} else {
+		// fmt.Fprintf(w, "Wrong credentials!")
 		t, _ := template.ParseFiles("register.html")
-		t.Execute(w, nil)
-	case "POST":
-		r.ParseForm()
-				
-		if checkRegistration(r.FormValue("username"), r.FormValue("password"), r.FormValue("password2")) == true {
-			// redirect to account page 
-			// http.Redirect(w, r, "/account/", http.StatusSeeOther)
-		} else {
-			fmt.Fprintf(w, "Wrong credentials!")
-			// http.Redirect(w, r, "/register/", http.StatusSeeOther)
-		}
-		fmt.Println("went POST")
-
-	default:
-		fmt.Fprintf(w, "Поддерживаются только GET и POST методы")
+		t.ExecuteTemplate(w, "register.html", "Wrong credentials!")
 	}
 }
 
-func accountPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is yout account!")
+func account(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is your account!")
 }
 
 func main() {
 	http.HandleFunc("/", mainPage)
-	http.HandleFunc("/login", loginPage)
-	http.HandleFunc("/register", registerPage)
-	http.HandleFunc("/account", accountPage)
+	http.HandleFunc("/login/", login)
+	http.HandleFunc("/register/", register)
+	http.HandleFunc("/registerResult/", registerResult)
+	http.HandleFunc("/account/", account)
 
 	err := http.ListenAndServe(":8080", nil) // устанавливаем порт для прослушивания
 	if err != nil {
